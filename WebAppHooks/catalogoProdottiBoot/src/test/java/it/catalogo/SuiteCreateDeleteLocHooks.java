f2a919787ef90922aad0073c7b4517e2e3c95737
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2021-10-02 12:50:31 +0200

    Update SuiteCreateDeleteLocHooks.java

--- WebAppHooks/catalogoProdottiBoot/src/test/java/it/catalogo/SuiteCreateDeleteLocHooks.java
+++ WebAppHooks/catalogoProdottiBoot/src/test/java/it/catalogo/SuiteCreateDeleteLocHooks.java
@@ -1,13 +1,3 @@
-
-//File risulta attualmente aggiornato per webdriver chrome headless!
-package it.catalogo.test;
-
-import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.concurrent.TimeUnit;
-
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Alert;
